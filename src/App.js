06ef8ca5db1abd27e14b1744f73faeb1bea24634
Erica Smith <ericasmith@Ericas-MacBook-Pro.local>
2021-02-25 20:42:16 -0800

    fixed indexing error

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
       </Button>
       </Header>
       <Body>
-        {memes.map(m=> <Meme src={m.images.fixed_width.url}/>)}
+        {memes.map(m=> <Meme key = {m.id} src={m.images.fixed_width.url}/>)}
       </Body>
     </Wrap>
   );
